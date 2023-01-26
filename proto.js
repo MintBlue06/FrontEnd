a0803a720d12be17545944e5bd86bb90e1ce5eee
MintBlue06 <120575827+MintBlue06@users.noreply.github.com>
2023-01-26 13:36:20 -0300

    Delete proto.js

--- proto.js
+++
@@ -1,7 +0,0 @@
-
-/ * Requiere id o class * /
-/ * Onclick 
-    Getelementbyclassname * /
-document.getElementById("divA").textContent = "Esto es un nuevo texto";
-
-document.getElementById("myImageId").src="newSource.png;
